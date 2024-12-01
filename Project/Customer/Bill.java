33a0547c726a81b82b2d343eb177acf5f6a177af
Nguyen Minh Khoa <145916505+KANguyen2802@users.noreply.github.com>
2024-12-01 13:04:00 +0700

    Bill.java

--- Project/Customer/Bill.java
+++ Project/Customer/Bill.java
@@ -2,14 +2,14 @@ package Project.Customer;
 
 import Project.Product.Laptop;
 import Project.Staff.Saler;
-//? import Project.Customer.Customer;
 
 public class Bill {
-
-    private Customer customer;
-    private String phoneNumber;
-    private Laptop laptop;
-    private Saler saler;
+    // todo Composition (tạo thuộc tính để lưu trữ đối tượng class khác) 
+    // todo (Mốiquan hệ HAS-A)
+    private Customer customer; // *Composition */
+    private String phoneNumber; // ? update tu Customer sang
+    private Laptop laptop; // *Composition */
+    private Saler saler; // *Composition */
     private String billId;
     private String date;
     private double totalAmount;
@@ -17,9 +17,11 @@ public class Bill {
     // * Constructor */
     public Bill(Customer customer, String phoneNumber, Laptop laptop, Saler saler, String billId, String date,
             double totalAmount) {
-        super();
+        // super(); 
+        //* Composition (tạo thuộc tính để lưu trữ đối tượng class khác) */
+        //* (Mối quan hệ HAS-A) */
         this.customer = customer;
-        this.phoneNumber = phoneNumber;
+        this.phoneNumber = customer.getPhoneNumber();
         this.laptop = laptop;
         this.saler = saler;
         this.billId = billId;
@@ -27,36 +29,40 @@ public class Bill {
         this.totalAmount = totalAmount;
     }
 
+    // ?Trả về đối tượng Customer mà Bill tham chiếu / (Lấy thông tin Customer)
     public Customer getCustomer() {
         return customer;
     }
 
+    // *Thiết lập đối tượng Customer */
     public void setCustomer(Customer customer) {
         this.customer = customer;
     }
-
-    // todo: update Customer phoneNumber sang String phoneNumber
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        this.phoneNumber = customer.getPhoneNumber();
     }
 
+    // ? Trả về đối tượng Laptop
     public Laptop getLaptop() {
         return laptop;
     }
 
+    // * */
     public void setLaptop(Laptop laptop) {
         this.laptop = laptop;
     }
 
+    // ? Trả về đối tượng Saler
     public Saler getSaler() {
         return saler;
     }
 
+    // * */
     public void setSaler(Saler saler) {
         this.saler = saler;
     }
@@ -87,15 +93,23 @@ public class Bill {
 
     @Override
     public String toString() {
-        return "Bill: " +
-                "Customer  " + customer +
-                ", Customer Phonenumber = " + phoneNumber +
-                "\n Laptop = " + laptop +
-                "\n Saler = " + saler +
-                "\n Bill ID = '" + billId + '\'' +
-                ", Date = '" + date + '\'' +
-                ", Total amount = " + totalAmount +
-                "}\n";
+        StringBuilder sb = new StringBuilder();
+        sb.append("+------------------------+------------------------------+\n");
+        sb.append("|         Field          |            Value             |\n");
+        sb.append("+------------------------+------------------------------+\n");
+        sb.append(String.format("| %-22s | %-28s |\n", "Customer", customer.getName()));
+        sb.append(String.format("| %-22s | %-28s |\n", "Customer Phonenumber", customer.getPhoneNumber()));
+        sb.append("+------------------------+------------------------------+\n");
+        sb.append(String.format("| %-22s | %-28s |\n", "Laptop", laptop.getName()));
+        sb.append(String.format("| %-22s | %-28s |\n", "Laptop", laptop.getModel()));
+        sb.append("+------------------------+------------------------------+\n");
+        sb.append(String.format("| %-22s | %-28s |\n", "Saler", saler.getName()));
+        sb.append("+------------------------+------------------------------+\n");
+        sb.append(String.format("| %-22s | %-28s |\n", "Bill ID", billId));
+        sb.append(String.format("| %-22s | %-28s |\n", "Date", date));
+        sb.append(String.format("| %-22s | %-28.2f |\n", "Total amount", totalAmount));
+        sb.append("+------------------------+------------------------------+");
+        return sb.toString();
     }
 
     public void toInfo() {
